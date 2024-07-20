9144f7e5aec9811b07226fe1cd3def04c373dc88
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-07-20 00:06:58 -0400

    Application Loop w/ Quit

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,33 @@
 const std = @import("std");
+const locale = @cImport({
+    @cInclude("locale.h");
+});
 const ncurses = @cImport({
     @cInclude("ncurses.h");
 });
 
 pub fn main() !void {
-    _ = ncurses.initscr();
-    _ = ncurses.printw("Application Window");
+    // Prepare locale for curses library calls
+    _ = locale.setlocale(locale.LC_ALL, "");
+
+    // Initialize curses screen
+    _ = ncurses.initscr().?;
+    _ = ncurses.keypad(ncurses.stdscr, true);
+    _ = ncurses.cbreak();
+    _ = ncurses.noecho();
+
+    _ = ncurses.printw("Entered application!");
     _ = ncurses.refresh();
-    _ = ncurses.getch();
+
+    // Application loop
+    while (true) {
+        const char: c_int = ncurses.getch();
+
+        if (char == 'q') {
+            break;
+        }
+    }
+
+    // Cleanup
     _ = ncurses.endwin();
 }
