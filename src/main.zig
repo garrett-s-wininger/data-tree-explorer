af5507c13b0fd84ebe6e84bf8fc36f360e386f5e
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-09-01 20:34:15 -0400

    Allow input from specified file

--- src/main.zig
+++ src/main.zig
@@ -2,30 +2,90 @@ const providers = @import("providers.zig");
 const std = @import("std");
 const terminal = @import("terminal.zig");
 
+fn applicationError(err: anyerror) !void {
+    std.debug.print("\n\n", .{});
+
+    switch (err) {
+        error.StreamTooLong => {
+            die("Retrieved JSON Data Larger than 1MB Limit");
+        },
+        error.SyntaxError => {
+            die("Data Not Valid JSON");
+        },
+        error.UnexpectedEndOfInput => {
+            die("Provided Data Incomplete, Unable to Parse");
+        },
+        else => {
+            return err;
+        },
+    }
+}
+
 fn die(message: []const u8) noreturn {
     std.debug.print("[ERROR] {s}\n", .{message});
     std.process.exit(1);
 }
 
-fn demo(allocator: std.mem.Allocator) !void {
-    try runWithData(allocator, &providers.example);
+fn demo(args: providers.ProviderArgs) !void {
+    try runWithData(args, &providers.example);
 }
 
-fn fromStdIn(allocator: std.mem.Allocator) !void {
-    try runWithData(allocator, &providers.stdIn);
+fn fromFile(args: providers.ProviderArgs) !void {
+    try runWithData(args, &providers.file);
 }
 
-fn runWithData(allocator: std.mem.Allocator, data_retriever: *const fn (std.mem.Allocator) anyerror!std.json.Parsed(std.json.Value)) !void {
-    var parsed = try data_retriever(allocator);
+fn parseArgs(allocator: std.mem.Allocator, source_file: *std.fs.File) !*const fn (providers.ProviderArgs) anyerror!void {
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    // Skip the executable for argument parsing
+    _ = args.next();
+
+    var action: *const fn (providers.ProviderArgs) anyerror!void = &usage;
+    var arg_count: u8 = 0;
+    var parse_as_file = false;
+    var required_args: u8 = 1;
+
+    while (args.next()) |arg| {
+        arg_count += 1;
+
+        if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+            action = &usage;
+        } else if (std.mem.eql(u8, arg, "--demo")) {
+            action = &demo;
+        } else if (std.mem.eql(u8, arg, "--")) {
+            action = &fromFile;
+        } else if (std.mem.eql(u8, arg, "-f") or std.mem.eql(u8, arg, "--file")) {
+            action = &fromFile;
+            required_args += 1;
+            parse_as_file = true;
+        } else if (parse_as_file) {
+            source_file.* = try std.fs.openFileAbsolute(arg, .{ .mode = .read_only });
+            parse_as_file = false;
+        }
+    }
+
+    // Output help data if inputs are inappropriate
+    if (arg_count != required_args) {
+        action = &usage;
+    }
+
+    return action;
+}
+
+fn runWithData(args: providers.ProviderArgs, provider: *const fn (providers.ProviderArgs) anyerror!std.json.Parsed(std.json.Value)) !void {
+    var parsed = try provider(args);
     defer parsed.deinit();
+    defer args.source.close();
 
-    try terminal.run(allocator, @ptrCast(&parsed.value.object));
+    try terminal.run(args.allocator, @ptrCast(&parsed.value.object));
 }
 
-fn usage(_: std.mem.Allocator) !void {
+fn usage(_: providers.ProviderArgs) !void {
     std.debug.print("data-tree-explorer v0.0.0\n", .{});
-    std.debug.print("Usage: data-tree-explorer --|-h|--help|--demo\n\n", .{});
+    std.debug.print("Usage: data-tree-explorer --|-f <FILE>|--file <FILE>|-h|--help|--demo\n\n", .{});
     std.debug.print("--: Use STDIN as the input data source\n", .{});
+    std.debug.print("-f, --file: Use FILE as the input data source\n", .{});
     std.debug.print("-h, --help: Print help information about this program\n", .{});
     std.debug.print("--demo: Run the application with a set of test JSON data as a demo\n", .{});
 }
@@ -40,48 +100,12 @@ pub fn main() !void {
         }
     }
 
-    // Parse Options
-    var args = try std.process.argsWithAllocator(allocator);
-    defer args.deinit();
+    var provider_args = .{ .allocator = allocator, .source = std.io.getStdIn() };
+    const action = parseArgs(allocator, &provider_args.source) catch {
+        die("Unable to parse application arguments");
+    };
 
-    // Skip the executable for argument parsing
-    _ = args.next();
-    var argCount: u8 = 0;
-    var action: *const fn (std.mem.Allocator) anyerror!void = &usage;
-
-    while (args.next()) |arg| {
-        argCount += 1;
-
-        if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help") or argCount > 1) {
-            action = &usage;
-        } else if (std.mem.eql(u8, arg, "--demo")) {
-            action = &demo;
-        } else if (std.mem.eql(u8, arg, "--")) {
-            action = &fromStdIn;
-        }
-    }
-
-    // Output help data if inputs are inappropriate
-    if (argCount != 1) {
-        action = &usage;
-    }
-
-    action(allocator) catch |err| {
-        std.debug.print("\n\n", .{});
-
-        switch (err) {
-            error.StreamTooLong => {
-                die("Retrieved JSON Data Larger than 1MB Limit");
-            },
-            error.SyntaxError => {
-                die("Data Not Valid JSON");
-            },
-            error.UnexpectedEndOfInput => {
-                die("Provided Data Incomplete, Unable to Parse");
-            },
-            else => {
-                return err;
-            },
-        }
+    action(provider_args) catch |err| {
+        try applicationError(err);
     };
 }
--- src/providers.zig
+++ src/providers.zig
@@ -2,25 +2,9 @@ const std = @import("std");
 
 const allocation_limit = 1_000_000;
 
-pub fn stdIn(allocator: std.mem.Allocator) !std.json.Parsed(std.json.Value) {
-    const in = std.io.getStdIn();
-    var reader = in.reader();
+pub const ProviderArgs = struct { allocator: std.mem.Allocator, source: std.fs.File };
 
-    const data = try reader.readAllAlloc(allocator, allocation_limit);
-    defer allocator.free(data);
-
-    const in_stat = try in.stat();
-
-    if (in_stat.kind == .named_pipe) {
-        in.close();
-        const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .mode = .read_write });
-        _ = std.os.linux.dup(tty.handle);
-    }
-
-    return std.json.parseFromSlice(std.json.Value, allocator, data, .{});
-}
-
-pub fn example(allocator: std.mem.Allocator) !std.json.Parsed(std.json.Value) {
+pub fn example(args: ProviderArgs) !std.json.Parsed(std.json.Value) {
     const json_data =
         \\{
         \\"stringKey": "value",
@@ -32,5 +16,23 @@ pub fn example(allocator: std.mem.Allocator) !std.json.Parsed(std.json.Value) {
         \\}
     ;
 
-    return std.json.parseFromSlice(std.json.Value, allocator, json_data, .{});
+    return std.json.parseFromSlice(std.json.Value, args.allocator, json_data, .{});
+}
+
+pub fn file(args: ProviderArgs) !std.json.Parsed(std.json.Value) {
+    const in = args.source;
+    var reader = in.reader();
+
+    const data = try reader.readAllAlloc(args.allocator, allocation_limit);
+    defer args.allocator.free(data);
+
+    const in_stat = try in.stat();
+
+    if (in_stat.kind == .named_pipe) {
+        in.close();
+        const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .mode = .read_only });
+        _ = std.os.linux.dup(tty.handle);
+    }
+
+    return std.json.parseFromSlice(std.json.Value, args.allocator, data, .{});
 }
